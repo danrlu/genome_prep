e1a53d3906a2b3a4bdee022c268e7c6d4e504854
Dan Lu <danlu1015@gmail.com>
2020-05-10 14:24:19 -0500

    fix download links

--- setup_genome.sh
+++ setup_genome.sh
@@ -1,16 +1,15 @@
-# Since wget and curl not working, downloaded manually from 
-#ftp://ftp.wormbase.org/pub/wormbase/releases/WS276/species/c_elegans/PRJNA13758/
-
-
 # This script will download, index, and prep reference genomes.
 
 # Prep N2 reference genome
 RELEASE=WS276
 PROJECT=PRJNA13758
 
+# Download and prepare FASTA Genome
+wget "ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/${PROJECT}/sequence/genomic/c_elegans.${PROJECT}.${RELEASE}.genomic.fa.gz"
+wget "ftp://ftp.wormbase.org/pub/wormbase/releases/${RELEASE}/species/c_elegans/${PROJECT}/c_elegans.${PROJECT}.${RELEASE}.annotations.gff3.gz"
 
-# # Download and prepare FASTA Genome
-# wget "ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/${PROJECT}/sequence/genomic/c_elegans.${PROJECT}.${RELEASE}.genomic.fa.gz"
+# If wget not working, can download manually from here:
+# ftp://ftp.wormbase.org/pub/wormbase/releases/WS276/species/c_elegans/PRJNA13758/
 
 
 bwa index c_elegans.${PROJECT}.${RELEASE}.genomic.fa.gz
@@ -23,6 +22,5 @@ gatk CreateSequenceDictionary -R c_elegans.${PROJECT}.${RELEASE}.genomic.fa -O c
 
 # to prep the gff3 for bcftools csq
 zcat c_elegans.${PROJECT}.${RELEASE}.annotations.gff3.gz | grep -P "\tWormBase\t" > c_elegans.${PROJECT}.${RELEASE}.annotations.WormBase.gff3
-
 
 Rscript setup_gff_CSQ.R c_elegans.${PROJECT}.${RELEASE}.annotations.WormBase.gff3
